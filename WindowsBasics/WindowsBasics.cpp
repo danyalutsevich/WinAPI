d193e2607c8d081ec135beb8a63afa86e4536554
luce_dp7x <luce_dp7x@72-8>
2021-11-18 14:51:48 +0200

    boxes

--- WindowsBasics/WindowsBasics.cpp
+++ WindowsBasics/WindowsBasics.cpp
@@ -299,12 +299,23 @@ void createControls(HWND);
 //////////////////////
 #define CMD_CHECKBOX 2001
 #define CMD_PUSHLIKE 2002
-
+#define CMD_RADIOBUTTON1 2003
+#define CMD_RADIOBUTTON2 2004
+#define CMD_RADIOBUTTON3 2005
+#define CMD_RADIOBUTTON4 2006
+#define CMD_COMBOBOX 2007
+#define CMD_COMBOBOX 2008
 
 //////////////////////
 
 HWND hwndCHECKBOX;
 HWND hwndPUSHLIKE;
+HWND hwndRADIO1;
+HWND hwndRADIO2;
+HWND hwndRADIO3;
+HWND hwndRADIO4;
+HWND hwndCOMBOBOX;
+HWND hwndLISTBOX;
 
 
 
@@ -391,6 +402,38 @@ LRESULT CALLBACK    WndProcControls(HWND hWnd, UINT message, WPARAM wParam, LPAR
 		}
 
 						 break;
+
+		case CMD_RADIOBUTTON1:
+
+
+			SendMessageW(hwndRADIO1, BM_SETCHECK, BST_CHECKED, 0);
+			SendMessageW(hwndRADIO2, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO3, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO4, BM_SETCHECK, BST_UNCHECKED, 0);
+
+
+			break;
+
+		case CMD_RADIOBUTTON2:
+
+			SendMessageW(hwndRADIO1, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO2, BM_SETCHECK, BST_CHECKED, 0);
+			SendMessageW(hwndRADIO3, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO4, BM_SETCHECK, BST_UNCHECKED, 0);
+
+			break;
+		case CMD_RADIOBUTTON3:
+			SendMessageW(hwndRADIO1, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO2, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO3, BM_SETCHECK, BST_CHECKED, 0);
+			SendMessageW(hwndRADIO4, BM_SETCHECK, BST_UNCHECKED, 0);
+			break;
+		case CMD_RADIOBUTTON4:
+			SendMessageW(hwndRADIO1, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO2, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO3, BM_SETCHECK, BST_UNCHECKED, 0);
+			SendMessageW(hwndRADIO4, BM_SETCHECK, BST_CHECKED, 0);
+			break;
 		}
 		break;
 	}
@@ -407,12 +450,29 @@ LRESULT CALLBACK    WndProcControls(HWND hWnd, UINT message, WPARAM wParam, LPAR
 void createControls(HWND hWnd) {
 
 
-	CreateWindowW(L"Button", L"Buttons", WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 10, 10, 380, 200, hWnd, NULL, hInst, 0);
+	CreateWindowW(L"Button", L"Buttons", WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 10, 10, 380, 220, hWnd, NULL, hInst, 0);
 	CreateWindowW(L"Button", L"Push", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 25, 35, 75, 23, hWnd, NULL, hInst, 0);
 	CreateWindowW(L"Button", L"Def", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON | WS_TABSTOP, 25, 65, 75, 23, hWnd, NULL, hInst, 0);
 	CreateWindowW(L"Button", L"PushLike", WS_CHILD | WS_VISIBLE | BS_PUSHLIKE, 25, 95, 75, 23, hWnd, NULL, hInst, 0);
 	hwndCHECKBOX = CreateWindowW(L"Button", L"unchecked", WS_CHILD | WS_VISIBLE | BS_CHECKBOX, 25, 125, 75, 23, hWnd, (HMENU)CMD_CHECKBOX, hInst, 0);
 	hwndPUSHLIKE = CreateWindowW(L"Button", L"unchecked", WS_CHILD | WS_VISIBLE | BS_CHECKBOX | BS_PUSHLIKE, 25, 155, 75, 23, hWnd, (HMENU)CMD_PUSHLIKE, hInst, 0);
+	CreateWindowW(L"Button", L"Right", WS_CHILD | WS_VISIBLE | BS_CHECKBOX | BS_RIGHTBUTTON, 25, 185, 75, 23, hWnd, NULL, hInst, 0);
+
+	hwndRADIO1 = CreateWindowW(L"Button", L"Radio1", WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 130, 35, 75, 23, hWnd, (HMENU)CMD_RADIOBUTTON1, hInst, 0);
+	hwndRADIO2 = CreateWindowW(L"Button", L"Radio2", WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 130, 65, 75, 23, hWnd, (HMENU)CMD_RADIOBUTTON2, hInst, 0);
+	hwndRADIO3 = CreateWindowW(L"Button", L"Radio3", WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 130, 95, 75, 23, hWnd, (HMENU)CMD_RADIOBUTTON3, hInst, 0);
+	hwndRADIO4 = CreateWindowW(L"Button", L"Radio4", WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON, 130, 125, 75, 23, hWnd, (HMENU)CMD_RADIOBUTTON4, hInst, 0);
+
+	hwndCOMBOBOX = CreateWindowW(L"Combobox", L"Combox", WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 130, 155, 75, 75, hWnd, (HMENU)CMD_COMBOBOX, hInst, 0);
+	SendMessageW(hwndCOMBOBOX, CB_ADDSTRING, 0, (LPARAM)L"a");
+	SendMessageW(hwndCOMBOBOX, CB_ADDSTRING, 0, (LPARAM)L"b");
+	SendMessageW(hwndCOMBOBOX, CB_ADDSTRING, 0, (LPARAM)L"c");
+
+	hwndLISTBOX = CreateWindowW(L"Listbox", L"Combox", WS_CHILD | WS_VISIBLE , 130, 185, 75, 75, hWnd, (HMENU)CMD_COMBOBOX, hInst, 0);
+	SendMessageW(hwndLISTBOX, LB_ADDSTRING, 0, (LPARAM)L"a");
+	SendMessageW(hwndLISTBOX, LB_ADDSTRING, 0, (LPARAM)L"b");
+	SendMessageW(hwndLISTBOX, LB_ADDSTRING, 0, (LPARAM)L"c");
+
 
 
 }
