15baed3b5345f62123d785b98f4a49cd60b7f18d
Луцевич Данило Олександрович <Luce_dp7x>
2021-11-23 14:50:36 +0200

    closeHandle

--- Threads/Threads.cpp
+++ Threads/Threads.cpp
@@ -7,15 +7,21 @@
 #define MAX_LOADSTRING 100
 #define CMD_BUTTON_1 1001
 #define CMD_BUTTON_2 1002
+#define CMD_BUTTON_3 1003
 
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+HWND listbox;
+
+
+
 
 HWND hwndBUTTON1;
 HWND hwndBUTTON2;
+HWND hwndBUTTON3;
 
 
 
@@ -28,45 +34,46 @@ INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 void StartThread();
 void StartThread2();
+void StartThread3();
 
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_THREADS, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_THREADS, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_THREADS));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_THREADS));
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -78,23 +85,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_THREADS));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_THREADS);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_THREADS));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 3);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_THREADS);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -109,20 +116,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -137,143 +144,196 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_CREATE:
+	switch (message)
+	{
+	case WM_CREATE:
 
-        hwndBUTTON1 = CreateWindowW(L"Button",L"Push me",WS_CHILD|WS_VISIBLE,10,10,75,23,hWnd,(HMENU)CMD_BUTTON_1,hInst,0);
-        hwndBUTTON2 = CreateWindowW(L"Button",L"Thread",WS_CHILD|WS_VISIBLE,50,50,75,23,hWnd,(HMENU)CMD_BUTTON_2,hInst,0);
-
-
-        break;
-
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-
-            case CMD_BUTTON_1:
-                StartThread();
+		hwndBUTTON1 = CreateWindowW(L"Button", L"Push me", WS_CHILD | WS_VISIBLE, 10, 10, 75, 23, hWnd, (HMENU)CMD_BUTTON_1, hInst, 0);
+		hwndBUTTON2 = CreateWindowW(L"Button", L"Thread", WS_CHILD | WS_VISIBLE, 10, 50, 75, 23, hWnd, (HMENU)CMD_BUTTON_2, hInst, 0);
+		hwndBUTTON3 = CreateWindowW(L"Button", L"percent", WS_CHILD | WS_VISIBLE, 10, 110, 75, 23, hWnd, (HMENU)CMD_BUTTON_3, hInst, 0);
+		listbox = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE, 90, 90, 300, 223, hWnd, (HMENU)NULL, hInst, 0);
 
 
-                break;
-            case CMD_BUTTON_2:
-                
-                StartThread2();
-                
-                
-                break;
-            case IDM_ABOUT:
-                /*DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);*/
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+		break;
+
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+
+		case CMD_BUTTON_1:
+			StartThread();
+
+
+			break;
+		case CMD_BUTTON_2:
+
+			StartThread2();
+
+
+			break;
+		case CMD_BUTTON_3:
+
+			StartThread3();
+
+			break;
+		case IDM_ABOUT:
+			/*DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);*/
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
 
 
 
-int fib(int n) {
-    
-    if (n==0) {
-        return 1;
-    }
-    if (n == 1) {
-        return 1;
-    }
-
-
-    fib(fib(n-1)+fib(n-2));
-}
 
 
 DWORD WINAPI ThreadProc(LPVOID params) {
 
-    while (1) {
 
-        MessageBoxW(NULL,L"hell",L"Works",MB_YESNOCANCEL);
-    }
 
-   // DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, About);
+		MessageBoxW(NULL, L"hell", L"Works", MB_YESNOCANCEL);
+	
 
-    return 0;
+	// DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), NULL, About);
+
+	return 0;
 }
 
 DWORD WINAPI ThreadProc2(LPVOID params) {
 
-    while (MessageBoxW(NULL, (LPCWSTR)params, L"Works", MB_YESNOCANCEL)) {
+	MessageBoxW(NULL, (LPCWSTR)params, L"Works", MB_YESNOCANCEL);
 
-        //MessageBoxW(NULL, (LPCWSTR)params,L"Works",MB_YESNOCANCEL);
-    }
+		//MessageBoxW(NULL, (LPCWSTR)params,L"Works",MB_YESNOCANCEL);
+	
 
 
-    return 0;
+	return 0;
 }
+
+
 
 void StartThread() {
 
-    CreateThread(
-        NULL, // attributes for thread
-        0,    //Stack limit 0 - system default
-        ThreadProc,// Address of thread function
-        NULL, // Pointer to parametr(s)
-        0,    // Creation flags
-        NULL  // Thread Id pointer
-        );
+	CreateThread(
+		NULL, // attributes for thread
+		0,    //Stack limit 0 - system default
+		ThreadProc,// Address of thread function
+		NULL, // Pointer to parametr(s)
+		0,    // Creation flags
+		NULL  // Thread Id pointer
+	);
 
 }
 
+
+
 void StartThread2() {
 
-    CreateThread(
-        NULL, 
-        0,    
-        ThreadProc2,
-        szTitle, 
-        0,   
-        NULL  
-    );
+	CreateThread(
+		NULL,
+		0,
+		ThreadProc2,
+		szTitle,
+		0,
+		NULL
+	);
+
+}
+
+
+float deposit = 100;
+//float percent = 2;
+
+struct DepData {
+
+	int month;
+	float percent;
+	DepData(int month, float percent) :month{ month }, percent{ percent } {
+
+	}
+};
+
+	HANDLE hts[12]; // threads handles;
+DWORD WINAPI ThreadProc3(LPVOID params) {
+	/*((DepData*)params)->month;
+	((DepData*)params)->percent;*/
+	wchar_t buff[100];
+
+	DepData* data = (DepData*)params;
+
+
+	deposit += deposit * (data->percent / 100.0f);
+	//_itow_s(deposit, buff, 10);
+
+	_snwprintf_s(buff, 100, L"month %d percent %.2f amount %.2f", data->month, data->percent, deposit);
+	SendMessage(listbox, LB_ADDSTRING, 100, (LPARAM)buff);
+
+
+	CloseHandle(hts[data->month]);
+
+	delete data;
+
+	
+	return 0;
+}
+
+//DepData* depdata = new DepData(10.f, 1.f);
+
+void StartThread3() {
+
+
+	wchar_t num[100];
+
+	for (int i = 0; i < 12; i++) {
+
+		_itow_s((int)CreateThread(NULL, 0, ThreadProc3, (LPVOID)new DepData(i+1, 10.f), 0, NULL),num,10);
+		SendMessage(listbox, LB_ADDSTRING, 100, (LPARAM)num);
+
+	}
+
 
 }
