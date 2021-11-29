4784e8696198350edc1729c21dc641e5c92b4296
Луцевич Данило Олександрович <Luce_dp7x>
2021-11-29 17:42:55 +0200

    dll

--- dll create/dllmain.cpp
+++ dll create/dllmain.cpp
@@ -1,5 +1,8 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define DLLEXPORT __declspec(dllexport)
 
@@ -12,6 +15,30 @@ extern "C" {
 		return x * x;
 
 	}
+
+	DLLEXPORT long cube(long x) {
+
+		return x * x * x;
+
+	}
+
+	DLLEXPORT long plus(long a, long b) {
+
+		return a + b;
+
+	}
+
+	DLLEXPORT wchar_t* dectosix(wchar_t* str) {
+
+		int x=_wtoi(str);
+
+		wchar_t buff[100];
+
+		_itow_s(x,buff,16);
+
+		return buff;
+	}
+
 
 }
 
--- dllS/dllS.cpp
+++ dllS/dllS.cpp
@@ -2,13 +2,22 @@
 //
 
 #include "framework.h"
+#include <string>
 #include "dllS.h"
 
 #define MAX_LOADSTRING 100
-#define CMD_BUTTON1 1000
+#define CMD_BUTTON_SQR 1001
+#define CMD_BUTTON_CUBE 1002
+#define CMD_BUTTON_PLUS 1003
+#define CMD_BUTTON_DECTOSIX 1004
 
 typedef long (*sqr_type)(long); // sqr_type ~~ long (*)(long)
+typedef long (*sqr_type2)(long,long); // sqr_type ~~ long (*)(long)
+typedef long (*sqr_type3)(wchar_t); // sqr_type ~~ long (*)(long)
 sqr_type sqr_fun; // sqr_type sqr_fun == long (*sqr_fun)(long)
+sqr_type cube_fun;
+sqr_type2 plus_fun;
+sqr_type3 dectosix_fun;
 
 
 // Global Variables:
@@ -16,8 +25,12 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-HWND button;
-
+HWND sqrB;
+HWND cubeB;
+HWND plusB;
+HWND dectosixB;
+HWND edit;
+HWND listbox;
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -25,7 +38,10 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-void buttonClick();
+void buttonSqr();
+void buttonCube();
+void buttonPlus();
+void dectosix();
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -85,7 +101,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DLLS));
 	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
 	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_DLLS);
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -137,7 +153,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 
-		button = CreateWindowW(L"Button", L"link", WS_CHILD | WS_VISIBLE, 10, 10, 75, 23, hWnd, (HMENU)CMD_BUTTON1, hInst, NULL);
+		sqrB = CreateWindowW(L"Button", L"sqr", WS_CHILD | WS_VISIBLE, 10, 10, 75, 23, hWnd, (HMENU)CMD_BUTTON_SQR, hInst, NULL);
+		cubeB = CreateWindowW(L"Button", L"cube", WS_CHILD | WS_VISIBLE, 10, 40, 75, 23, hWnd, (HMENU)CMD_BUTTON_CUBE, hInst, NULL);
+		plusB = CreateWindowW(L"Button", L"plus", WS_CHILD | WS_VISIBLE, 10, 70, 75, 23, hWnd, (HMENU)CMD_BUTTON_PLUS, hInst, NULL);
+		dectosixB = CreateWindowW(L"Button", L"dectosix", WS_CHILD | WS_VISIBLE, 10, 100, 75, 23, hWnd, (HMENU)CMD_BUTTON_DECTOSIX, hInst, NULL);
+		listbox = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE, 100, 10, 300, 400, hWnd, NULL, hInst, NULL);
+		edit = CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE, 10, 130, 75, 23, hWnd, NULL, hInst, NULL);
+
 		break;
 	case WM_COMMAND:
 	{
@@ -147,10 +169,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (wmId)
 		{
 
-		case CMD_BUTTON1:
-			buttonClick();
+		case CMD_BUTTON_SQR:
+			buttonSqr();
 			break;
+		case CMD_BUTTON_PLUS:
+			buttonPlus();
+			break;
+		case CMD_BUTTON_CUBE:
+			buttonCube();
+			break;
+		case CMD_BUTTON_DECTOSIX:
 
+			dectosix();
+			break;
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
@@ -200,7 +231,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 }
 
 
-void buttonClick() {
+void buttonSqr() {
 
 	HMODULE dll = LoadLibraryW(L"..\\Debug\\dll create.dll");
 	if (dll == NULL) {
@@ -212,19 +243,176 @@ void buttonClick() {
 
 		if (sqr_fun) {
 
-			wchar_t buff[100];
-			long param = 2;
-			_snwprintf_s(buff,100,L"%d^2 = %d",param, sqr_fun(param));
 
-			MessageBoxW(NULL, buff, L"OK", MB_OK | MB_ICONINFORMATION);
+
+
+			wchar_t buff[100];
+			SendMessageW(edit, WM_GETTEXT, MAX_LOADSTRING, (LPARAM)buff);
+			long param = _wtoi(buff);
+			_snwprintf_s(buff, 100, L"%d^3 = %d", param, sqr_fun(param));
+			SendMessageW(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)buff);
+
+
+			//MessageBoxW(NULL, buff, L"OK", MB_OK | MB_ICONINFORMATION);
 
 		}
 		else {
 
-			MessageBoxW(NULL, L"error", L"proc", MB_OK | MB_ICONERROR);
+			MessageBoxW(NULL, L"error", L"sqr", MB_OK | MB_ICONERROR);
 		}
-		MessageBoxW(NULL, L"ok", L"dll", MB_OK | MB_ICONINFORMATION);
+		
 	}
 
+
+
+
+}
+void buttonCube() {
+
+
+
+	HMODULE dll = LoadLibraryW(L"..\\Debug\\dll create.dll");
+	if (dll == NULL) {
+
+		MessageBoxW(NULL, L"error", L"dll", MB_OK | MB_ICONERROR);
+	}
+	else {
+		cube_fun = (sqr_type)GetProcAddress(dll, "cube");
+		if (cube_fun) {
+			wchar_t buff[100];
+			SendMessageW(edit,WM_GETTEXT, MAX_LOADSTRING, (LPARAM)buff);
+			long param = _wtoi(buff);
+			_snwprintf_s(buff, 100, L"%d^3 = %d", param, cube_fun(param));
+			SendMessageW(listbox,LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)buff);
+
+
+			//MessageBoxW(NULL, buff, L"OK", MB_OK | MB_ICONINFORMATION);
+		}
+		else {
+
+			MessageBoxW(NULL, L"error", L"cube", MB_OK | MB_ICONERROR);
+
+
+		}
+
+	}
+}
+
+
+int parts;
+std::string* splitString(std::string str, char sym) {
+
+	size_t pos = 0;
+	parts = 1;
+
+	while ((pos = str.find(sym, pos + 1)) != std::string::npos) {
+		parts++;
+	}
+
+	std::string* res = new std::string[parts];
+	pos = 0;
+	size_t pos2;
+
+	for (int i = 0; i < parts - 1; i++) {
+
+		pos2 = str.find(sym, pos + 1);
+		res[i] = str.substr(pos, pos2 - pos);
+		pos = pos2;
+
+
+	}
+
+	res[parts - 1] = str.substr(pos + 1);
+
+
+
+	return res;
+}
+
+
+
+
+void buttonPlus() {
+	try {
+
+	HMODULE dll = LoadLibraryW(L"..\\Debug\\dll create.dll");
+	if (dll == NULL) {
+
+		MessageBoxW(NULL, L"error", L"dll", MB_OK | MB_ICONERROR);
+	}
+	else {
+		plus_fun = (sqr_type2)GetProcAddress(dll, "plus");
+		if (plus_fun) {
+
+			std::string* params;
+			wchar_t buff[100];
+			SendMessageW(edit, WM_GETTEXT, MAX_LOADSTRING, (LPARAM)buff);
+			params=splitString(std::to_string((char)buff), '+');
+
+			long a = std::atoi(params[0].c_str());
+			SendMessageW(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)L"A");
+			SendMessageA(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)params[0].c_str());
+			long b = std::atoi(params[1].c_str());
+			SendMessageW(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)L"B");
+			SendMessageA(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)params[1].c_str());
+			_snwprintf_s(buff, 100, L"%d + %d = %d", a,b, plus_fun(1,1));
+			SendMessageW(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)buff);
+
+
+
+			//MessageBoxW(NULL, buff, L"OK", MB_OK | MB_ICONINFORMATION);
+		}
+		else {
+
+			MessageBoxW(NULL, L"error", L"cube", MB_OK | MB_ICONERROR);
+
+
+		}
+
+
+	}
+
+	}
+	catch (std::exception& ex) {
+
+		SendMessageA(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)ex.what());
+	}
+
+}
+
+
+void dectosix() {
+	try{
+	HMODULE dll = LoadLibraryW(L"..\\Debug\\dll create.dll");
+	if (dll == NULL) {
+
+		MessageBoxW(NULL, L"error", L"dll", MB_OK | MB_ICONERROR);
+	}
+	else {
+		dectosix_fun = (sqr_type3)GetProcAddress(dll, "dectosix");
+		if (dectosix_fun) {
+			wchar_t buff[100];
+			SendMessageW(edit, WM_GETTEXT, MAX_LOADSTRING, (LPARAM)buff);
+			long param = _wtoi(buff);
+			_snwprintf_s(buff, 100, L"%d to hex = %d", param, dectosix_fun(param));
+			SendMessageW(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)buff);
+
+
+			//MessageBoxW(NULL, buff, L"OK", MB_OK | MB_ICONINFORMATION);
+		}
+		else {
+
+			MessageBoxW(NULL, L"error", L"cube", MB_OK | MB_ICONERROR);
+
+
+		}
+
+	}
+
+}
+catch (std::exception& ex) {
+
+	SendMessageA(listbox, LB_ADDSTRING, MAX_LOADSTRING, (LPARAM)ex.what());
+}
 
 }
