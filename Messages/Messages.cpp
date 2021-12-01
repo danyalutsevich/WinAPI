c356416f1c1efbb13e3b14125876bc42e15e2a1b
Луцевич Данило Олександрович <Luce_dp7x>
2021-12-01 17:10:37 +0200

    mouse&timer

--- Messages/Messages.cpp
+++ Messages/Messages.cpp
@@ -5,14 +5,22 @@
 #include "Messages.h"
 
 #define MAX_LOADSTRING 100
+#define CMD_TIMER_MOVEWINDOW 2000
+#define CMD_TIMER_ANTICLICK 2001
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-HWND hStatic;
+HWND hStatic1;
+HWND hStatic2;
+HWND hStatic3;
+HWND hProgres;
+
 HWND listbox;
+WCHAR str[100];
+
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -21,41 +29,41 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	// TODO: Place code here.
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_MESSAGES, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_MESSAGES, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MESSAGES));
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MESSAGES));
 
-    MSG msg;
+	MSG msg;
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
-    return (int) msg.wParam;
+	return (int)msg.wParam;
 }
 
 
@@ -67,23 +75,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEXW wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MESSAGES));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+2);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MESSAGES);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MESSAGES));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_MESSAGES);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -98,20 +106,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, 800, 800, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	if (!hWnd)
+	{
+		return FALSE;
+	}
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
-   return TRUE;
+	return TRUE;
 }
 
 //
@@ -124,77 +132,175 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - post a quit message and return
 //
 //
+int count = 0;
+int x = 0;
+int y = 0;
+bool xFlag = false;
+bool yFlag = false;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-        case WM_CREATE:
-            
-            hStatic = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 10, 10, 50, 15, hWnd, 0, hInst, NULL);
-            listbox = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 10, 30, 400, 300, hWnd, 0, hInst, NULL);
+	switch (message)
+	{
+	case WM_CREATE:
 
-            break;
-    case WM_KEYDOWN:
+		hStatic1 = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 10, 10, 100, 15, hWnd, 0, hInst, NULL);
+		hStatic2 = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 120, 10, 100, 15, hWnd, 0, hInst, NULL);
+		hStatic3 = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 230, 10, 100, 15, hWnd, 0, hInst, NULL);
+		listbox = CreateWindowW(L"Listbox", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 10, 30, 400, 300, hWnd, 0, hInst, NULL);
+		hProgres = CreateWindowW(PROGRESS_CLASSW, L"", WS_CHILD | WS_VISIBLE | PBS_MARQUEE | PBS_SMOOTH, 10, 350, 100, 15, hWnd, 0, hInst, NULL);
+		SetTimer(hWnd, CMD_TIMER_ANTICLICK, 1000, NULL);
+		SetTimer(hWnd, CMD_TIMER_MOVEWINDOW, 10, NULL);
+		//SendMessageW(hProgres,PBM_SETSTEP,10,0);
 
-        WCHAR str[100];
+		break;
 
-        _snwprintf_s(str,100,L"%d %u",wParam, (lParam >> 16) & 0xff);
-        //(lParam >> 16) & 0x11111111;
-        SendMessageW(hStatic,WM_SETTEXT,100,(LPARAM)str);
-        SendMessageW(listbox,LB_ADDSTRING,100,(LPARAM)str);
+	case WM_TIMER:
+		if (wParam == CMD_TIMER_ANTICLICK) {
 
 
-        break;
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+			SendMessageW(hProgres, PBM_DELTAPOS, -4, 0);
+
+
+		}
+		if (wParam == CMD_TIMER_MOVEWINDOW) {
+
+
+			MoveWindow(hWnd, x, y, 800, 800, 1);
+
+			if (xFlag) {
+				x -= 6;
+
+			}
+			else {
+				x += 6;
+			}
+			if (yFlag) {
+				y -= 6;
+
+			}
+			else {
+				y += 6;
+			}
+
+			if (x > 1920 - 800) {
+				xFlag = true;
+
+			}
+
+			if (x < 0) {
+				xFlag = false;
+
+			}
+
+			if (y > 1080 - 800) {
+				yFlag = true;
+
+			}
+
+			if (y < 0) {
+				yFlag = false;
+
+			}
+
+		}
+
+		break;
+	case WM_KEYDOWN:
+	{
+
+		_snwprintf_s(str, 100, L"%d %u", wParam, (lParam >> 16) & 0xff);
+		//(lParam >> 16) & 0x11111111;
+		SendMessageW(hStatic1, WM_SETTEXT, 100, (LPARAM)str);
+		SendMessageW(listbox, LB_ADDSTRING, 100, (LPARAM)str);
+	}
+
+	break;
+	case WM_MBUTTONDOWN:
+
+		KillTimer(hWnd,CMD_TIMER_ANTICLICK);
+		break;
+	case WM_MOUSEMOVE:
+
+		_snwprintf_s(str, 100, L"%d %d", GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
+
+		SendMessageW(hStatic2, WM_SETTEXT, 100, (LPARAM)str);
+		SendMessageW(listbox, LB_ADDSTRING, 100, (LPARAM)str);
+
+
+		break;
+	case WM_LBUTTONDOWN:
+
+		_snwprintf_s(str, 100, L"%d", count);
+		SendMessageW(hStatic3, WM_SETTEXT, 100, (LPARAM)str);
+
+		count++;
+
+
+		break;
+	case WM_RBUTTONUP:
+		SendMessageW(hProgres, PBM_DELTAPOS, 4, 0);
+		SendMessageW(hProgres, PBM_SETBARCOLOR, 0, RGB(rand() % 255, rand() % 255, rand() % 255));
+
+		break;
+	case WM_LBUTTONUP:
+
+		SendMessageW(hProgres, PBM_DELTAPOS, -4, 0);
+		SendMessageW(hProgres, PBM_SETBARCOLOR, 0, RGB(rand() % 255, rand() % 255, rand() % 255));
+
+
+
+		break;
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
--- Messages/framework.h
+++ Messages/framework.h
@@ -8,6 +8,8 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+#include <windowsx.h>
+#include <CommCtrl.h>
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
