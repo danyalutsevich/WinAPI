0c3d4a0cbc7bc6c4600ea244baceb05361f92ad3
danyalutsevich <luchevich31@gmail.com>
2021-12-01 23:11:02 +0200

    clicker

---
+++ clicker/clicker.cpp
@@ -0,0 +1,272 @@
+// Practice.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "clicker.h"
+
+#define MAX_LOADSTRING 100
+#define CMD_BUTTON_CLICKER 1000
+#define CMD_CPS 1001
+#define CMD_REDUCE_PROGRESS 1002
+
+
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+HWND clicker;
+HWND scoreS;
+HWND CPS;
+HWND hProgres;
+
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_CLICKER, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CLICKER));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+COLORREF color;
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+
+	srand(time(0));
+	color = RGB(rand() % 255, rand() % 255, rand() % 255);
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_CLICKER));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)CreateSolidBrush(color);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_CLICKER);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, 1080 / 2, 1920 / 2, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//F
+//
+
+int score = 0;
+int clicksPerSec = 0;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+
+	case WM_CREATE: {
+
+
+		const WCHAR fontName[] = L"Cascadia Mono";
+		const long nFontSize = 50;
+
+		HFONT s_hFont;
+		HDC hdc = GetDC(hWnd);
+		LOGFONT logFont = { 0 };
+		logFont.lfHeight = -MulDiv(nFontSize, GetDeviceCaps(hdc, LOGPIXELSY), 72);
+		logFont.lfWeight = FW_BOLD;
+		wcscpy_s(logFont.lfFaceName, fontName);
+		s_hFont = CreateFontIndirect(&logFont);
+
+
+		const long nFontSize2 = 15;
+
+		HFONT s_hFont2;
+		HDC hdc2 = GetDC(hWnd);
+		LOGFONT logFont2 = { 0 };
+		logFont2.lfHeight = -MulDiv(nFontSize2, GetDeviceCaps(hdc2, LOGPIXELSY), 72);
+		logFont2.lfWeight = FW_BOLD;
+		wcscpy_s(logFont2.lfFaceName, fontName);
+		s_hFont2 = CreateFontIndirect(&logFont2);
+
+
+		clicker = CreateWindowW(L"Button", L"", WS_CHILD | WS_VISIBLE, 10, 300, 505, 505, hWnd, (HMENU)CMD_BUTTON_CLICKER, hInst, NULL);
+		scoreS = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 0, 0, 525, 100, hWnd, NULL, hInst, NULL);
+		CPS = CreateWindowW(L"Static", L"", WS_CHILD | WS_VISIBLE | SS_CENTER, 520 / 2 - 50, 130, 100, 23, hWnd, NULL, hInst, NULL);
+		hProgres = CreateWindowW(PROGRESS_CLASSW, L"", WS_CHILD | WS_VISIBLE | PBS_MARQUEE | PBS_SMOOTH, 0, 100, 524, 15, hWnd, 0, hInst, NULL);
+		SendMessageW(hProgres, PBM_SETRANGE, -5, MAKELPARAM(0, 100));
+		SendMessageW(hProgres, PBM_SETBARCOLOR, 0, color);
+
+
+		SetTimer(hWnd, CMD_CPS, 1000, NULL);
+		SetTimer(hWnd, CMD_REDUCE_PROGRESS, 1000, NULL);
+
+		SendMessage(scoreS, WM_SETFONT, (WPARAM)s_hFont, (LPARAM)MAKELONG(TRUE, 0));
+
+
+
+		SendMessage(CPS, WM_SETFONT, (WPARAM)s_hFont2, (LPARAM)MAKELONG(TRUE, 0));
+
+		break;
+	}
+
+	case WM_TIMER:
+		if (wParam == CMD_CPS) {
+
+
+			char buff[100];
+			_itoa_s(clicksPerSec, buff, 10);
+			SendMessageA(CPS, WM_SETTEXT, 0, (LPARAM)buff);
+			clicksPerSec = 0;
+
+		}
+		if (wParam == CMD_REDUCE_PROGRESS) {
+
+			SendMessageW(hProgres, PBM_DELTAPOS, -5, 0);
+
+		}
+		break;
+
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case CMD_BUTTON_CLICKER:
+			score += SendMessageW(hProgres, PBM_DELTAPOS, 1, 0) + 1;
+			char buff[100];
+			_itoa_s(score, buff, 10);
+			SendMessageA(scoreS, WM_SETTEXT, 0, (LPARAM)buff);
+
+
+
+			clicksPerSec++;
+
+			break;
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
